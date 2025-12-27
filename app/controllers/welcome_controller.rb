9d11956492f62d41d888de2aa2ece46ec7bfd740
Matheus Lima <matheuslima@duck>
2025-12-27 13:37:47 -0300

    Adiciona novas funcionalidades e melhorias na interface do sistema de carteira de criptomoedas

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,6 @@
 class WelcomeController < ApplicationController
   def index
+    @nome = params[:nome]
+    @curso = "Curso de Ruby on Rails"
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+    def dataBR(dataUS)
+        dataUS.strftime("%d/%m/%Y")
+    end
+    def anoBR(anoBR)
+        anoBR.strftime("%Y")
+    end
+    def name_app
+        "Crypto Wallet"
+    end
+    def ambiente_rails
+        if Rails.env.development?
+            "Desenvolvimento"
+        elsif Rails.env.production?
+            "Produção"
+        else Rails.env.test?
+            "Teste"
+        end
+    end
+    def welcome_message
+        "Seja bem vindo ao nosso site de criação de moedas"
+    end
 end
